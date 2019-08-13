e77c50476ac85f782c37f9fd89f383911c3d3dce
Dzmitry_Kastjuchenka <dzmitry_kastjuchenka@epam.com>
2019-08-13 22:38:05 +0300

    some changes

--- src/test/java/TestGetRequestJson.java
+++ src/test/java/TestGetRequestJson.java
@@ -1,5 +1,4 @@
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
@@ -16,14 +15,21 @@ public class TestGetRequestJson extends BaseTest {
                 param("location", "-33.8670522,151.1957362").
                 param("key", "AIzaSyCUuzmM0CkPCDMlxufkIIfgL_N1dRmlPis").
                 param("radius", "500").
+                log().all().
                 when().get("/maps/api/place/nearbysearch/json").
                 then().assertThat().statusCode(200)
                 //.and().contentType(ContentType.JSON)
                 //.and().body("results[0].name", equalTo("Sydney"))
                 .and().body("results[0].place_id", equalTo("ChIJP3Sa8ziYEmsRUKgyFmh9AQM"))
                 .and().header("Server", "scaffolding on HTTPServer2")
+                .log().body()
                 .extract().response();
         JsonPath jsonPath = rawToJson(resp);
         int count = jsonPath.get("results.size()");
+        /*for (int i = 0; i < count; i ++){
+            System.out.println(jsonPath.get("results[" + i + "].name"));
+        }*/
+        System.out.println(jsonPath.get("results.name"));
+
     }
 }
