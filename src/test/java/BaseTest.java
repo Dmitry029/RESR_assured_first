1830747af1e4ee36d118d077d3de0b01b94a8cef
Dzmitry_Kastjuchenka <dzmitry_kastjuchenka@epam.com>
2019-08-14 11:09:07 +0300

    Add static and dynamic working with Json.

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,6 +2,10 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.path.xml.XmlPath;
 import io.restassured.response.Response;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class BaseTest {
 
     public XmlPath rawToXml(Response response){
@@ -10,5 +14,9 @@ public class BaseTest {
 
     public JsonPath rawToJson(Response response){
         return new JsonPath(response.asString());
+    }
+
+    public static String GenerateStringFromResource(String path) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(path)));
     }
 }
