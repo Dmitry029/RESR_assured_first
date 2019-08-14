e7f8fdf37acae490336e43d3d1f13426a6c91e03
Dzmitry_Kastjuchenka <dzmitry_kastjuchenka@epam.com>
2019-08-14 11:08:12 +0300

    Add static and dynamic working with Json.

--- src/test/java/DynamicJson.java
+++ src/test/java/DynamicJson.java
@@ -1,19 +1,20 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class DynamicJson extends BaseTest {
 
-    @Test
-    public void addBook() {
+    @Test(dataProvider = "BookData")
+    public void addBook(String isbm, String aisle) {
 
         RestAssured.baseURI = "http://216.10.245.166";
         Response resp = (Response) given()
             .header("Content-Type", "application/json")
-            .body(Payload.addBookBodyJson("ffff", "6554"))
+            .body(Payload.addBookBodyJson(isbm, aisle))
             .when()
             .post("Library/Addbook.php")
             .then().assertThat().statusCode(200)
@@ -22,5 +23,13 @@ public class DynamicJson extends BaseTest {
         String id = js.get("ID");
         System.out.println("ID " + id);
 
+    }
+
+    @DataProvider(name="BookData")
+    public Object[][] getData(){
+        return new Object[][]{
+                {"firstData","123"},
+                {"secondData", "456"},
+                {"thirdData", "789"}};
     }
 }
